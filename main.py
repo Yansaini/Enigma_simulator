6dae3cc2bbc931b893adb570fe0672d8d3064fa7
Gemma <gemmalisseth.vaca@estudiant.enti.cat>
2025-12-08 18:49:50 +0100

    Comentarios de ayuda para funcion leer_rueda

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ def leer_rueda(nombre_archivo):
     try:
         with open(nombre_archivo, "r") as f:
             lineas = f.readlines()
-        conexiones = lineas[0].strip().upper() 
-        punto_avance = "Z"  
+        conexiones = lineas[0].strip().upper() # Como cambia cada letra
+        punto_avance = "Z"   # Valor por defecto
         if len(lineas) > 1:
             punto_avance = lineas[1].strip().upper()
         return conexiones, punto_avance
     except FileNotFoundError:
         print(f"Error: Archivo {nombre_archivo} no encontrado")
-        # Mapeo
+        # Si no hay archivo, cada letra se transforma en ella misma (mapeo)
         return "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "Z"
 
 
