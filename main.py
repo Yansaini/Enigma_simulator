25f802d03c72f939ccae094f90d4624a850275d0
Gemma <gemmalisseth.vaca@estudiant.enti.cat>
2025-12-08 18:38:10 +0100

    Auto stash before merge of "Develop" and "origin/Develop"

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+<<<<<<< Updated upstream
+=======
+
+def leer_rueda(nombre_archivo):
+    try:
+        with open(nombre_archivo, "r") as f:
+            lineas = f.readlines()
+        conexiones = lineas[0].strip().upper() 
+        punto_avance = "Z"  
+        if len(lineas) > 1:
+            punto_avance = lineas[1].strip().upper()
+        return conexiones, punto_avance
+    except FileNotFoundError:
+        print(f"Error: Archivo {nombre_archivo} no encontrado")
+        # Mapeo
+        return "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "Z"
+
+>>>>>>> Stashed changes
 #Menu principal :)
 
 def main():
