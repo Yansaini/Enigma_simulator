16457ba097285f6cbb52f8fd7acb126c304e5b4f
Yansaini <yansul.yansul@estudiant.enti.cat>
2025-12-09 19:33:07 +0100

    updated version

--- main.py
+++ main.py
@@ -61,56 +61,6 @@ def limpiar_texto(texto):
     return resultado
 
 
-
-def leer_rueda(nombre_archivo):
-    try:
-        with open(nombre_archivo, "r") as f:
-            lineas = f.readlines()
-        conexiones = lineas[0].strip().upper() # Como cambia cada letra
-        punto_avance = "Z"   # Valor por defecto
-        if len(lineas) > 1:
-            punto_avance = lineas[1].strip().upper()
-        return conexiones, punto_avance
-    except FileNotFoundError:
-        print(f"Error: Archivo {nombre_archivo} no encontrado")
-        # Si no hay archivo, cada letra se transforma en ella misma (mapeo)
-        return "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "Z"
-
-
-def cifrar_letra(letra, mapeo, posicion):
-    abecedario = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    #1. Mirar que numero es la letra (A=0, B=1, etc)
-    indice = (abecedario.index(letra) + posicion) % 26
-    #2. Cifrar usando el mapeo de la rueda
-    letra_cifrada = mapeo[indice]
-    #3. Convertir de vuelta a posicion normal
-    indice_salida = (abecedario.index(letra_cifrada) - posicion) % 26
-    return abecedario[indice_salida]
-
-
-def descifrar_letra(letra, mapeo, posicion):
-    abecedario = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    indice = (abecedario.index(letra) + posicion) % 26
-    # Esto hace el mapeo inverso (es decir, si A->E, entonces E->A)
-    inverso = [""] * 26
-    for i in range(26):
-        inverso[abecedario.index(mapeo[i])] = abecedario[i]
-    mapeo_inverso = "".join(inverso)  # Para convertir la lista a string
-    letra_cifrada = mapeo_inverso[indice]
-    indice_salida = (abecedario.index(letra_cifrada) - posicion) % 26
-    return abecedario[indice_salida]
-
-
-#this funcion clean the text bfore encrypting it
-def limpiar_texto(texto):
-    texto = texto.upper() # convert everything in uppercase
-    resultado = ""
-    for ch in texto:
-        if "A" <= ch <= "Z": #only keep letter from A to Z
-            resultado += ch
-    return resultado
-
-
 #submenu functions of option 3.1
 def ver_rotor():
     print("Que rotor quieres ver? (1, 2, 3)")
